2b03f2d0fa1063db4809e053c17c5ea6f0100256
Kaushan <kaushandutta5@gmail.com>
2024-02-03 20:05:20 +0530

    fixes

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-const express=require('express');
-const http=require('http');
-const cors=require('cors');
-const { graphqlHTTP } = require("express-graphql");
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { graphqlHTTP } = require('express-graphql');
 
-const app=express();
-const server=http.createServer(app);
-require("dotenv").config();
+const app = express();
+const server = http.createServer(app);
+require('dotenv').config();
 
 app.use(express.static('public'));
-app.use(cors(
-    {
-        credentials:true
-    }
-));
+app.use(
+  cors({
+    origin: 'https://campus-buddy-f5pyl4w8s-kaushan-dutta.vercel.app',
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-app.use("/graphql", graphqlHTTP({
-    schema:require('./Schema/schema.js'),
-    graphiql: true 
+app.use('/graphql', graphqlHTTP({
+  schema: require('./Schema/schema.js'),
+  graphiql: true,
 }));
-app.get('/',(req,res)=>{
-    res.status(200).json({message:'Welcome to campus-buddy'});
-})
-const port=process.env.PORT || 8080;
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to campus-buddy' });
+});
 
-server.listen(port,()=>{
-    console.log(`Server listening on port ${port}`);
+const port = process.env.PORT || 8080;
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
